fca80fdf617a19039bcadb4c97173a895f91d92e
Jan Kowalleck <jan.kowalleck@gmail.com>
2020-03-14 13:17:14 +0100

    config checker

---
+++ bin/check_configs.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+CONFIG_DIR="$(dirname "$0")/../config"
+echo "CONFIG_DIR: ${CONFIG_DIR}"
+
+for config_file in "${CONFIG_DIR}"/*
+do
+  echo "* check config file: $(basename "${config_file}")"
+  nichtparasoup config --check "$config_file" || exit $?
+done
